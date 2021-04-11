0ba09b2a71a33b510214bc9ba107baae20468317
Samuel Chibueze <sechibueze@gmail.com>
2021-04-11 12:22:27 +0100

    fixed endpoints

--- models/ShilohRegistration.js
+++ models/ShilohRegistration.js
@@ -1,41 +1,45 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
-const ShilohRegistrationSchema = new Schema({
-  member: {
-    type: Schema.Types.ObjectId,
-    required: true,
-    unique: true,
-    ref: 'Member'
+const ShilohRegistrationSchema = new Schema(
+  {
+    member: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      unique: true,
+      ref: "Member",
+    },
+    profile: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      unique: true,
+      ref: "Profile",
+    },
+    access: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    otp: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    availability: {
+      type: Array,
+      default: [],
+      required: true,
+    },
+    accomodation: {
+      type: String,
+      required: true,
+      default: "",
+    },
   },
-  profile: {
-    type: Schema.Types.ObjectId,
-    required: true,
-    unique: true,
-    ref: 'Profile'
-  },
-  accessId: {
-    type: String,
-    unique: true,
-    required: true
-  },
-  otp: {
-    type: String,
-    unique: true,
-    required: true
-  },
-  availability: {
-    type: Array,
-    default:[],
-    required: true
-  },
-  accomodation: {
-    type: String,
-    required: true,
-    default: ''
-  }
-}, { timestamps: true });
+  { timestamps: true }
+);
 
-
-
-module.exports = ShilohRegistration = mongoose.model('ShilohRegistration', ShilohRegistrationSchema);
+module.exports = ShilohRegistration = mongoose.model(
+  "ShilohRegistration",
+  ShilohRegistrationSchema
+);
--- routes/shilohEventRoute.js
+++ routes/shilohEventRoute.js
@@ -1,58 +1,57 @@
-const express = require('express');
-const { check } = require('express-validator');
-const checkMember = require('../middlewares/checkMember');
-const checkAdmin = require('../middlewares/checkAdmin');
+const express = require("express");
+const { check } = require("express-validator");
+const checkMember = require("../middlewares/checkMember");
+const checkAdmin = require("../middlewares/checkAdmin");
 const router = express.Router();
-const { 
+const {
   handleShilohEventRegistration,
-  getAllShilohEventAttendees,
+  getAllShilohEventRegistrations,
   getShilohAttendeeById,
   updateShilohAttendeeById,
   removeShilohAttendeeById,
-  generateShilohAttendanceReport
- } = require('../controllers/ShilohEventControllers');
+  generateShilohAttendanceReport,
+} = require("../controllers/ShilohEventControllers");
 
 /**
  * @route GET /api/shiloh/register
  * @desc Register for shiloh
  * @access public
  */
-router.post('/register', checkMember, handleShilohEventRegistration);
+router.post("/register", checkMember, handleShilohEventRegistration);
 
 /**
  * @route GET /api/shiloh/attendees
  * @desc Get a list of all shiloh attendees
  * @access private && admin
  */
-router.get('/attendees', checkMember, checkAdmin, getAllShilohEventAttendees);
+router.get("/attendees", checkMember, getAllShilohEventRegistrations);
 
 /**
  * @route GET /api/shiloh/attendees/:accessId
  * @desc Get Attendee by ID
  * @access private && admin
  */
-router.get('/attendees/:accessId', getShilohAttendeeById);
+router.get("/attendees/:accessId", getShilohAttendeeById);
 
 /**
  * @route PUT /api/shiloh/attendees/:accessId
  * @desc Remove Attendee
  * @access private && admin
  */
+router.put("/attendees/update", checkMember, updateShilohAttendeeById);
 
-router.put('/attendees/:accessId', checkMember, updateShilohAttendeeById);
 /**
  * @route PUT /api/shiloh/report
  * @desc Attendance report
  * @access private && admin
  */
+router.get("/report", checkMember, checkAdmin, generateShilohAttendanceReport);
 
-router.get('/report', checkMember, checkAdmin, generateShilohAttendanceReport);
 /**
  * @route PUT /api/shiloh/attendees/:accessId
  * @desc Remove Attendee
  * @access private && admin
  */
-
-router.delete('/attendees/:accessId', checkMember, checkAdmin, removeShilohAttendeeById);
+router.put("/attendees", checkMember, removeShilohAttendeeById);
 
 module.exports = router;
